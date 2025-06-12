ec7d774cfd3aff164a2011dd25308b3c9f49528b
Justine Okumu <okumujustine01@gmail.com>
2025-06-11 23:17:49 -0500

    adds project setup with promting functionality

---
+++ code/main.py
@@ -0,0 +1,136 @@
+import logging
+import os
+
+from db_ingestion import embed_documents, get_db_collection
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from paths import APP_CONFIG_FPATH, OUTPUTS_DIR, PROMPT_CONFIG_FPATH
+from prompt_builder import build_prompt_from_config
+from utils import load_yaml_config
+
+logger = logging.getLogger()
+
+
+def setup_logging():
+    logger.setLevel(logging.INFO)
+
+    # File handler
+    file_handler = logging.FileHandler(os.path.join(OUTPUTS_DIR, "rag_assistant.log"))
+    file_handler.setLevel(logging.INFO)
+
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
+load_dotenv()
+
+# To avoid tokenizer parallelism warning from huggingface
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+collection = get_db_collection(collection_name="publications")
+
+
+def retrieve_relevant_documents(
+    query: str,
+    n_results: int = 5,
+    threshold: float = 0.3,
+) -> list[str]:
+    """
+    Query the ChromaDB database with a string query.
+
+    Args:
+        query (str): The search query string
+        n_results (int): Number of results to return (default: 5)
+        threshold (float): Threshold for the cosine similarity score (default: 0.3)
+
+    Returns:
+        dict: Query results containing ids, documents, distances, and metadata
+    """
+    logging.info(f"Retrieving relevant documents for query: {query}")
+    relevant_results = {
+        "ids": [],
+        "documents": [],
+        "distances": [],
+    }
+    # Embed the query using the same model used for documents
+    logging.info("Embedding query...")
+    query_embedding = embed_documents([query])[0]  # Get the first (and only) embedding
+
+    logging.info("Querying collection...")
+    # Query the collection
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=n_results,
+        include=["documents", "distances"],
+    )
+
+    logging.info("Filtering results...")
+    keep_item = [False] * len(results["ids"][0])
+    for i, distance in enumerate(results["distances"][0]):
+        if distance < threshold:
+            keep_item[i] = True
+
+    for i, keep in enumerate(keep_item):
+        if keep:
+            relevant_results["ids"].append(results["ids"][0][i])
+            relevant_results["documents"].append(results["documents"][0][i])
+            relevant_results["distances"].append(results["distances"][0][i])
+
+    return relevant_results["documents"]
+
+
+def respond_to_query(
+    prompt_config: dict,
+    query: str,
+    llm: str,
+    n_results: int = 5,
+    threshold: float = 0.3,
+) -> str:
+    """
+    Respond to a query using the ChromaDB database.
+    """
+
+    relevant_documents = retrieve_relevant_documents(
+        query, n_results=n_results, threshold=threshold
+    )
+
+    input_data = (
+        f"Relevant documents:\n\n{relevant_documents}\n\nUser's question:\n\n{query}"
+    )
+
+    rag_assistant_prompt = build_prompt_from_config(
+        prompt_config, input_data=input_data
+    )
+
+    llm = ChatGroq(model=llm)
+
+    response = llm.invoke(rag_assistant_prompt)
+    return response.content
+
+
+if __name__ == "__main__":
+    setup_logging()
+    app_config = load_yaml_config(APP_CONFIG_FPATH)
+    prompt_config = load_yaml_config(PROMPT_CONFIG_FPATH)
+
+    rag_assistant_prompt = prompt_config["rag_assistant_prompt"]
+
+    vectordb_params = app_config["vectordb"]
+    llm = app_config["llm"]
+
+    query = "What is Patch Embedding"
+
+    response = respond_to_query(
+        prompt_config=rag_assistant_prompt,
+        query=query,
+        llm=llm,
+        **vectordb_params,
+    )
+    logging.info("-" * 100)
+    logging.info("LLM response:")
+    logging.info(response + "\n\n")
